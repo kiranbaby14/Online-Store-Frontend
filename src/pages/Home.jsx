b52ef895adf0da0dc7334a8f4d70c3503cc60d80
kiranbaby14 <kiranbaby256@gmail.com>
2022-02-18 23:01:36 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Slider />
+    </>
+
+  )
+}
+
+export default Home
