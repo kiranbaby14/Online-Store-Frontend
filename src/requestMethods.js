73e7aab6483e73731db069580394470c68bce053
kiranbaby14 <kiranbaby256@gmail.com>
2022-03-24 20:30:52 +0530

    website complete

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzBiOTkwMWM2ZTdmMDllY2Q5MmI1ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NzM2MDQ0MCwiZXhwIjoxNjQ3NjE5NjQwfQ.EtL3RqVGR8O4KWG28nfQCFqgGN6Y528_DDpPYbVMTJk";
+const TOKEN =  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
